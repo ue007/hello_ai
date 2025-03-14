8a435ae8f61a8d78541aa042c27225e0708d5c36
jeff.fu <ue007@webgpu.info>
2025-03-14 14:35:04 +0000

    fix: mlp bug

--- d2l/03_chapter_multilayer-perceptrons/mlp.py
+++ d2l/03_chapter_multilayer-perceptrons/mlp.py
@@ -14,13 +14,43 @@
 
 # 激活函数
 
+# import torch
+# from d2l import torch as d2l
+
+# # ReLU函数 最受欢迎的激活函数是修正线性单元（Rectified linear unit，ReLU）
+# x = torch.arange(-8.0, 8.0, 0.1, requires_grad=True)
+# y = torch.relu(x)
+# d2l.plot(x.detach(), y.detach(), 'x', 'relu(x)', figsize=(5, 2.5))
+
+# # sigmoid函数
+# y = torch.sigmoid(x)
+# d2l.plot(x.detach(), y.detach(), 'x', 'sigmoid(x)', figsize=(5, 2.5))
+
+# # 清除以前的梯度
+# x.grad.data.zero_()
+# y.backward(torch.ones_like(x),retain_graph=True)
+# d2l.plot(x.detach(), x.grad, 'x', 'grad of sigmoid', figsize=(5, 2.5))
+
+
+# # tanh函数
+# y = torch.tanh(x)
+# d2l.plot(x.detach(), y.detach(), 'x', 'tanh(x)', figsize=(5, 2.5))
+
+# # 清除以前的梯度
+# x.grad.data.zero_()
+# y.backward(torch.ones_like(x),retain_graph=True)
+# d2l.plot(x.detach(), x.grad, 'x', 'grad of tanh', figsize=(5, 2.5))
+
 import torch
 from d2l import torch as d2l
 
-# ReLU函数 最受欢迎的激活函数是修正线性单元（Rectified linear unit，ReLU）
+# ReLU函数
 x = torch.arange(-8.0, 8.0, 0.1, requires_grad=True)
 y = torch.relu(x)
 d2l.plot(x.detach(), y.detach(), 'x', 'relu(x)', figsize=(5, 2.5))
+
+y.backward(torch.ones_like(x), retain_graph=True)
+d2l.plot(x.detach(), x.grad, 'x', 'grad of relu', figsize=(5, 2.5))
 
 # sigmoid函数
 y = torch.sigmoid(x)
@@ -31,7 +61,6 @@ x.grad.data.zero_()
 y.backward(torch.ones_like(x),retain_graph=True)
 d2l.plot(x.detach(), x.grad, 'x', 'grad of sigmoid', figsize=(5, 2.5))
 
-
 # tanh函数
 y = torch.tanh(x)
 d2l.plot(x.detach(), y.detach(), 'x', 'tanh(x)', figsize=(5, 2.5))
@@ -40,3 +69,5 @@ d2l.plot(x.detach(), y.detach(), 'x', 'tanh(x)', figsize=(5, 2.5))
 x.grad.data.zero_()
 y.backward(torch.ones_like(x),retain_graph=True)
 d2l.plot(x.detach(), x.grad, 'x', 'grad of tanh', figsize=(5, 2.5))
+
+print(x,y)
